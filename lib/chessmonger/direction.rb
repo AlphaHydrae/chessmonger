648627adfb651e3f6f335a92d4e387b7f55e24b6
AlphaHydrae <hydrae.alpha@gmail.com>
2012-09-05 22:29:39 +0200

    Implementation for latest Direction spec.

--- lib/chessmonger/direction.rb
+++ lib/chessmonger/direction.rb
@@ -3,6 +3,26 @@ module Chessmonger
 
   class Direction
 
+    def self.between origin, target
+      
+      dx, dy = target.x - origin.x, target.y - origin.y
+      return nil if dx == 0 and dy == 0
+
+      if dx.abs == dy.abs
+        if dx + dy != 0
+          dx + dy >= 1 ? NE : SW
+        else
+          dx - dy >= 1 ? SE : NW
+        end
+      elsif dx == 0
+        dy >= 1 ? N : S
+      elsif dy == 0
+        dx >= 1 ? E : W
+      else
+        nil
+      end
+    end
+
     def initialize dx, dy
       raise 'dx must be an integer' unless dx.kind_of? Fixnum
       raise 'dy must be an integer' unless dy.kind_of? Fixnum
@@ -13,6 +33,13 @@ module Chessmonger
       board.pos pos.x + distance * @dx, pos.y + distance * @dy
     end
 
+    def rotate n45 = 1
+      i = ALL.index self
+      return nil unless i
+      i = (i + n45) % 8
+      ALL[i < 0 ? 8 + i : i ]
+    end
+
     N = Direction.new 0, 1
     NE = Direction.new 1, 1
     E = Direction.new 1, 0
@@ -21,5 +48,9 @@ module Chessmonger
     SW = Direction.new -1, -1
     W = Direction.new -1, 0
     NW = Direction.new -1, 1
+
+    private
+
+    ALL = [ N, NE, E, SE, S, SW, W, NW ]
   end
 end

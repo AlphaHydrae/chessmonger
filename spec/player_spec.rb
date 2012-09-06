2c4fce7dc5ca29188bb06d5be68cc3c4f8249bf2
AlphaHydrae <hydrae.alpha@gmail.com>
2012-09-06 21:44:50 +0200

    Fixed and improved player spec.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,16 +2,22 @@
 describe 'Player' do
 
   it "should be initializable with a string" do
-    lambda{ Player.new 'John Doe' }.should_not raise_error
+    lambda{ Chessmonger::Player.new 'John Doe' }.should_not raise_error
   end
 
   it "should have a name" do
-    Player.new('John Doe').name.should == 'John Doe'
+    Chessmonger::Player.new('John Doe').name.should == 'John Doe'
   end
 
   it "should only accept a string as argument" do
     [ nil, false, true, Object.new, [], {}, :symbol, -2, 0, 3, 4.5 ].each do |invalid|
-      lambda{ Player.new invalid }.should raise_error
+      lambda{ Chessmonger::Player.new invalid }.should raise_error
+    end
+  end
+
+  it "should not accept empty strings as argument" do
+    [ '', ' ', "\t", "\n" ].each do |invalid|
+      lambda{ Chessmonger::Player.new invalid }.should raise_error
     end
   end
 end

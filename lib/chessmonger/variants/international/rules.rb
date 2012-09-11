d4dc33fa55da947a06c8ca2c6c1bc024f2c7d853
AlphaHydrae <hydrae.alpha@gmail.com>
2012-09-11 22:06:09 +0200

    Added king to international chess board setup.

--- lib/chessmonger/variants/international/rules.rb
+++ lib/chessmonger/variants/international/rules.rb
@@ -6,12 +6,18 @@ module Chessmonger
     class InternationalChess < BasicRules
 
       def setup game
+
         board = game.board
+
         pawn = Behavior::ChessPawn.new
         8.times do |i|
           board.put Piece.new(pawn, game.players[0]), board.pos(i + 1, 2)
           board.put Piece.new(pawn, game.players[1]), board.pos(i + 1, 7)
         end
+
+        king = Behavior::ChessKing.new
+        board.put Piece.new(king, game.players[0]), board.pos(5, 1)
+        board.put Piece.new(king, game.players[1]), board.pos(4, 8)
       end
     end
   end
--- spec/variants/international/rules_spec.rb
+++ spec/variants/international/rules_spec.rb
@@ -14,40 +14,44 @@ describe 'InternationalChess' do
   its_players_should_play_in_turn
   its_two_players_should_move_north_and_south
 
-  describe '#setup' do
+  it "should set up the pieces correctly" do
 
-    before :each do
-      @rules.setup @game
-    end
+    @rules.setup @game
+    board = @game.board
 
-    it "should put nothing on ranks 3 through 6" do
-      4.times do |i|
-        8.times do |j|
-          pos = @game.board.pos j + 1, i + 3
-          @game.board.get(pos).should be_nil
-          @game.board.get(pos).should be_nil
-        end
+    # rank 1
+    #8.times{ |i| board.get(board.pos(i + 1, 1)).player.should be(@game.players[0]) }
+    board.get(board.pos(5, 1)).behavior.should be_a_kind_of(Chessmonger::Behavior::ChessKing)
+
+    # pawns on rank 2
+    8.times do |i|
+      pos = @game.board.pos i + 1, 2
+      @game.board.get(pos).tap do |piece|
+        piece.player.should be(@game.players[0])
+        piece.behavior.should be_a_kind_of(Chessmonger::Behavior::ChessPawn)
       end
     end
 
-    it "should put player 1 pawns on rank 2" do
-      8.times do |i|
-        pos = @game.board.pos i + 1, 2
-        @game.board.get(pos).tap do |piece|
-          piece.player.should be(@game.players[0])
-          piece.behavior.should be_a_kind_of(Chessmonger::Behavior::ChessPawn)
-        end
+    # nothing on ranks 3 through 6
+    4.times do |i|
+      8.times do |j|
+        pos = @game.board.pos j + 1, i + 3
+        @game.board.get(pos).should be_nil
+        @game.board.get(pos).should be_nil
       end
     end
 
-    it "should put player 2 pawns on rank 7" do
-      8.times do |i|
-        pos = @game.board.pos i + 1, 7
-        @game.board.get(pos).tap do |piece|
-          piece.player.should be(@game.players[1])
-          piece.behavior.should be_a_kind_of(Chessmonger::Behavior::ChessPawn)
-        end
+    # pawns on rank 7
+    8.times do |i|
+      pos = @game.board.pos i + 1, 7
+      @game.board.get(pos).tap do |piece|
+        piece.player.should be(@game.players[1])
+        piece.behavior.should be_a_kind_of(Chessmonger::Behavior::ChessPawn)
       end
     end
+
+    # rank 8
+    #8.times{ |i| board.get(board.pos(i + 1, 1)).player.should be(@game.players[1]) }
+    board.get(board.pos(4, 8)).behavior.should be_a_kind_of(Chessmonger::Behavior::ChessKing)
   end
 end

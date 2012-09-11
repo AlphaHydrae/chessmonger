dc6a8d682ca024aa3c84b033701f659a3947763e
AlphaHydrae <hydrae.alpha@gmail.com>
2012-09-11 08:31:49 +0200

    Minor correction.

--- lib/chessmonger/variants/international/chess_pawn.rb
+++ lib/chessmonger/variants/international/chess_pawn.rb
@@ -12,7 +12,9 @@ module Chessmonger
 
         # basic move
         target = dir.from board, origin
-        yield self.class.move piece, origin, target if target and board.get(target).nil?
+        if target and board.get(target).nil?
+          yield Chessmonger::Move.new piece.player, piece, origin, target
+        end
 
         # diagonal attacks
         [ -1, 1 ].each do |rot|
@@ -20,14 +22,10 @@ module Chessmonger
           if target
             capture = board.get target
             if capture and game.enemy_piece? piece.player, capture
-              yield self.class.move piece, origin, target, capture
+              yield Chessmonger::Move.new piece.player, piece, origin, target, capture
             end
           end
         end
-      end
-
-      def self.move piece, origin, target, capture = nil
-        Chessmonger::Move.new piece.player, piece, origin, target, capture
       end
     end
   end
